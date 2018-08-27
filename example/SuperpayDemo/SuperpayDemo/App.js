d417020ec7740c0f6a77e1e42ae808976fe832a4
zhangkai <Recheng123>
2018-08-27 21:28:40 +0800

    完善android支付

--- example/SuperpayDemo/SuperpayDemo/App.js
+++ example/SuperpayDemo/SuperpayDemo/App.js
@@ -26,14 +26,16 @@ export default class App extends Component<Props> {
 
             if(Platform.OS==='android'){
                 NativeModules.SupayAndroidManager.pay('{"appid":"wxeae61855935702fb","noncestr":"f8b3dacef9c146f8a8e55e0f4630be5e","package":"Sign=WXPay","partnerid":"232028432","prepayid":"wx06200512915760d46c7c6ac32367430420","sign":"597ED7F160249C28A26021121DC1C919","timestamp":"1533557113"}',"WechatPay", (info)=>{
-                    if (info.returnCode === '0'){
+                    console.log(info)
+                    if (info === '0'){
                         console.log('支付成功')
-                    }else if (info.returnCode === '2'){
+                    }else if (info === '2'){
                         console.log('支付取消')
                     }else {
-                        console.log('Error:',info.errorDes)
+                        console.log('Error:',info)
                     }
                 });
+
             }else{
                 NativeModules.SupayManager.createWXPayment('{"appid":"wxeae61855935702fb","noncestr":"f8b3dacef9c146f8a8e55e0f4630be5e","package":"Sign=WXPay","partnerid":"232028432","prepayid":"wx06200512915760d46c7c6ac32367430420","sign":"597ED7F160249C28A26021121DC1C919","timestamp":"1533557113"}', (info)=>{
                     if (info.errorCode === 0){
@@ -49,12 +51,12 @@ export default class App extends Component<Props> {
           <TouchableOpacity  onPress={() => {
               if(Platform.OS==='android'){
                   NativeModules.SupayAndroidManager.pay('{"appid":"wxeae61855935702fb","noncestr":"f8b3dacef9c146f8a8e55e0f4630be5e","package":"Sign=WXPay","partnerid":"232028432","prepayid":"wx06200512915760d46c7c6ac32367430420","sign":"597ED7F160249C28A26021121DC1C919","timestamp":"1533557113"}',"AliPay", (info)=>{
-                    if (info.returnCode === '0'){
+                    if (info === '0'){
                         console.log('支付成功')
-                    }else if (info.returnCode === '2'){
+                    }else if (info === '2'){
                         console.log('支付取消')
                     }else {
-                        console.log('Error:',info.errorDes)
+                        console.log('Error:',info)
                     }
                   })
               }else{
--- example/SuperpayDemo/SuperpayDemo/android/app/src/main/java/com/superpaydemo/SupayAndroidManager.java
+++ example/SuperpayDemo/SuperpayDemo/android/app/src/main/java/com/superpaydemo/SupayAndroidManager.java
@@ -3,7 +3,6 @@ package com.superpaydemo;
 import android.widget.Toast;
 
 import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -33,43 +32,43 @@ public class SupayAndroidManager extends ReactContextBaseJavaModule {
      * @param payWay 支付路径
      */
     @ReactMethod
-    public void pay(String payInfo, String payWay, final Promise info) {
+    public void pay(String payInfo, String payWay, final Callback  callback) {
+
         PayParams.Builder payBuilder = new PayParams.Builder(getCurrentActivity());
-        info.reject("returnCode", String.valueOf(0));
-        return;
-//        switch (payWay) {
-//            case "WechatPay":
-//                payBuilder.payWay(PayWay.WechatPay);
-//                //微信支付包名签名必须和官网一致  请注意!!!
-//                payBuilder.wechatAppID("填入你的微信appid");
-//                break;
-//            case "AliPay":
-//                payBuilder.payWay(PayWay.AliPay);
-//                break;
-//        }
-//        PayParams payParams = payBuilder.payInfo(payInfo).build();
-//        ChengPay.newInstance(payParams).doPay(new OnPayResultListener() {
-//            @Override
-//            public void onPaySuccess(PayWay payWay) {
-//                LogUtil.i("支付成功 " + payWay.toString());
-//                info.reject("returnCode", String.valueOf(0));
-//                return;
-//            }
-//
-//            @Override
-//            public void onPayCancel(PayWay payWay) {
-//                LogUtil.i("支付取消 " + payWay.toString());
-//                info.reject("returnCode", String.valueOf(2));
-//                return;
-//            }
-//
-//            @Override
-//            public void onPayFailure(PayWay payWay, int errCode) {
-//                LogUtil.i("支付失败 " + payWay.toString() + errCode);
-//                info.reject("returnCode", String.valueOf(errCode));
-//                return;
-//            }
-//        });
+        switch (payWay) {
+           case "WechatPay":
+               payBuilder.payWay(PayWay.WechatPay);
+               //微信支付包名签名必须和官网一致  请注意!!!
+               payBuilder.wechatAppID("wx6c94cedf0b0fe9f9");
+               break;
+           case "AliPay":
+               payBuilder.payWay(PayWay.AliPay);
+               break;
+       }
+        PayParams payParams = payBuilder.payInfo(payInfo).build();
+        ChengPay.newInstance(payParams).doPay(new OnPayResultListener() {
+           @Override
+           public void onPaySuccess(PayWay payWay) {
+               LogUtil.i("支付成功 " + payWay.toString());
+               callback.invoke(String.valueOf(0));
+               return;
+           }
+
+           @Override
+           public void onPayCancel(PayWay payWay) {
+               LogUtil.i("支付取消 " + payWay.toString());
+               callback.invoke(String.valueOf(2));
+               return;
+           }
+
+           @Override
+           public void onPayFailure(PayWay payWay, int errCode) {
+               LogUtil.i("支付失败 " + payWay.toString() + errCode);
+               callback.invoke(String.valueOf(errCode));
+               return;
+           }
+       });
+
     }
 
 
